6f9668dd4041cbca30820047e6bd9c8ca53955c7
Dawid Chatkiewicz <dawid.chatkieiwcz@gmail.com>
2023-07-13 08:53:49 +0200

    changing css

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
-const span = document.querySelector('.hover-btn2')
-        const overlay = document.querySelector('.overlay')
+const span = document.querySelector('.hover-btn2');
+const overlay = document.querySelector('.overlay');
+const container = document.querySelector('.hover-container')
+const hoverContainer = document.querySelector('.hover-container')
 
-        window.addEventListener('mousemove', (e) => {
-            const { clientX, clientY } = e;
-            const x = Math.round((clientX / window.innerWidth) * 100);
-            const y = Math.round((clientY / window.innerHeight) * 100);
+window.addEventListener('mousemove', e => {
+	const { clientX, clientY } = e;
+	const x = Math.round((clientX / window.innerWidth) * 100);
+	const y = Math.round((clientY / window.innerHeight) * 100);
 
-            gsap.to(overlay, {
-                '--x': `${x}%`,
-                '--y': `${y}%`,
-                duration: 0.4,
-                ease: 'sine.out'
-            })
-        })
+	gsap.to(overlay, {
+		'--x': `${x}%`,
+		'--y': `${y}%`,
+		duration: 0.4,
+		ease: 'sine.out',
+	});
+});
 
-        span.addEventListener('click', () => {
-            overlay.classList.toggle('is-open')
-        })
+
+
+span.addEventListener('click', () => {
+	overlay.classList.toggle('is-open');
+});
